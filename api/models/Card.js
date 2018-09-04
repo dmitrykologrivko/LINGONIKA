441f5e814ad7223021e73c1e1ec907036e94dc3b
Dmitry Kologrivko <dmitrykologrivko@gmail.com>
2018-09-04 17:48:16 +0300

    Added blank models and controllers

---
+++ api/models/Card.js
@@ -0,0 +1,17 @@
+/**
+ * Card.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+
+
+  },
+
+};
+
