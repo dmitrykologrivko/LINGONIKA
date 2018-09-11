8a6ffc8f715fe56118d0a2db6fe6cc6478c4ffd3
Dmitry Kologrivko <dmitrykologrivko@gmail.com>
2018-09-11 14:37:54 +0300

    Implementing hashing password in register user action

--- api/controllers/users/register-user.js
+++ api/controllers/users/register-user.js
@@ -2,11 +2,16 @@ const schema = require('../../schemas/validation/user-schema');
 
 module.exports = async (req, res) => {
 
+  // Validate a user and check if email already used
   const {isValid, fields} = await sails.helpers.validate(req.body, schema.register);
   if (isValid) return res.badRequest(fields);
 
-  let user = await User.create(req.body).fetch();
+  // Hash a password
+  const {hash} = await sails.helpers.password.hashPassword(req.body.password);
 
-  res.status(201).json(user);
+  // Create a user
+  let user = await User.create({...req.body, hashedPassword: hash}).fetch();
+
+  return res.status(201).json(user);
 
 };
