e6de4600ee0de8d1d357594f0b9f302a970feb4e
Dmitry Kologrivko <dmitrykologrivko@gmail.com>
2018-09-19 11:52:26 +0300

    Added meta-data implementation for groups api

--- api/controllers/groups/meta-data.js
+++ api/controllers/groups/meta-data.js
@@ -1,10 +1,23 @@
 const LANGUAGES = require('../../chooses/languages');
 
-
 module.exports = async (req, res) => {
 
+  let countLearnedWords = 0;
+
+  if (req.body && (req.body.fromLanguage && req.body.toLanguage)) {
+    countLearnedWords = await Card.count({
+      user: req.me.id,
+      isLearned: true,
+      fromLanguage: req.body.fromLanguage,
+      toLanguage: req.body.toLanguage
+    });
+  } else {
+    countLearnedWords = await Card.count({user: req.me.id, isLearned: true});
+  }
+
   return res.ok({
-    languages: LANGUAGES
+    languages: LANGUAGES,
+    countLearnedWords: countLearnedWords
   });
 
 };
