7699bec2ccb8485f23ef754ad480c6537e514da1
Dmitry Kologrivko <dmitrykologrivko@gmail.com>
2018-09-23 15:44:45 +0300

    Added groups meta data

--- server/models/group.js
+++ server/models/group.js
@@ -4,13 +4,34 @@ const LANGUAGES = require('../constants/languages');
 
 module.exports = function(Group) {
   applyValidationRules(Group);
-  disableRemoteMethods(Group)
+  applyRemoteMethods(Group);
+  disableRemoteMethods(Group);
 };
 
 function applyValidationRules(Group) {
   Group.validatesLengthOf('name', {max: 100});
   Group.validatesInclusionOf('fromLanguage', {in: Object.keys(LANGUAGES)});
   Group.validatesInclusionOf('toLanguage', {in: Object.keys(LANGUAGES)});
+}
+
+function applyRemoteMethods(Group) {
+  Group.meta = meta;
+
+  Group.remoteMethod('meta', {
+    description: 'Get meta data.',
+    http: {
+      path: '/meta',
+      verb: 'get'
+    },
+    accepts: [
+      {arg: 'fromLanguage', type: 'string'},
+      {arg: 'toLanguage', type: 'string'},
+    ],
+    returns: [
+      {arg: 'languages', type: 'object'},
+      {arg: 'countLearnedWords', type: 'number'}
+    ]
+  });
 }
 
 function disableRemoteMethods(Group) {
@@ -31,4 +52,17 @@ function disableRemoteMethods(Group) {
   Group.disableRemoteMethodByName('prototype.__get__cards');
   Group.disableRemoteMethodByName('prototype.__updateById__cards');
   Group.disableRemoteMethodByName('prototype.__count__cards');
+}
+
+async function meta(fromLanguage, toLanguage) {
+  const Card = this.app.models.Card;
+
+  let countLearnedWords = 0;
+  if (fromLanguage && toLanguage) {
+    countLearnedWords = await Card.count({isLearned: true, fromLanguage: fromLanguage, toLanguage: toLanguage});
+  } else {
+    countLearnedWords = await Card.count({isLearned: true});
+  }
+
+  return [LANGUAGES, countLearnedWords];
 }
