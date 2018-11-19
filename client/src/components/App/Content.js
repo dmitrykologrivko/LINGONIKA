91248c76d2df415303519b33ed637c83a7b8d7f2
dmitrykologrivko <dmitrykologrivko@gmail.com>
2018-11-19 17:50:11 +0300

    Added basic components

---
+++ client/src/components/App/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Content.css';
+
+class Content extends React.Component {
+  render() {
+    return (
+      <section></section>
+    )
+  }
+}
+
+export default Content;
