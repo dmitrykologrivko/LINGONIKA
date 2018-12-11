f3f88b759b76f9ae332186c862393e0061f92bc8
dmitrykologrivko <dmitrykologrivko@gmail.com>
2018-12-11 12:05:31 +0300

    Refactor reducers

--- client/src/components/Groups/list/CreateGroupBox.js
+++ client/src/components/Groups/list/CreateGroupBox.js
@@ -58,7 +58,7 @@ class CreateGroupBox extends React.Component {
   }
 }
 
-const mapStateToProps = state => ({...state.groups.common, ...state.groups.createGroupBox});
+const mapStateToProps = state => ({...state.groups.groupsMeta, ...state.groups.createGroupBox});
 
 const mapDispatchToProps = dispatch => {
   return bindActionCreators({
