d90ad6d3a59157e54cc883c48b2b6bf93bebc3b2
dmitrykologrivko <dmitrykologrivko@gmail.com>
2018-12-15 17:55:49 +0300

    Remove using redux: Header component

--- client/src/actions/appActions.js
+++
@@ -1,7 +0,0 @@
-export const CHANGE_PROFILE_MENU_VISIBILITY = 'app/change-profile-menu-visibility';
-
-export function changeProfileMenuVisibility() {
-  return {
-    type: CHANGE_PROFILE_MENU_VISIBILITY
-  }
-}
--- client/src/reducer.js
+++ client/src/reducer.js
@@ -1,11 +1,9 @@
 import {combineReducers} from 'redux';
 import {connectRouter} from 'connected-react-router';
 
-import app from './reducers/appReducer';
 import groups from './reducers/groupsReducer';
 
 export default history => combineReducers({
   router: connectRouter(history),
-  app,
   groups
 });
