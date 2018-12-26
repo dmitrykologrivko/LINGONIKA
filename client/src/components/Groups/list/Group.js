d4c59147395a1f93a226383b4d87df4b90a5a182
Dmitry Kologrivko <dmitrykologrivko@gmail.com>
2018-12-26 14:57:44 +0300

    Added rename group function

--- client/src/components/Groups/list/Group.js
+++ client/src/components/Groups/list/Group.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 
 import './Group.css';
 
+const GROUP_NAME_MAX_LENGTH = 50;
+
 class Group extends React.Component {
   state = {
-    isActionsMenuVisible: false
+    isActionsMenuVisible: false,
+    isEditGroupFormVisible: false,
+    editableGroupName: null
   };
 
   componentDidMount() {
@@ -17,30 +21,74 @@ class Group extends React.Component {
   }
 
   render() {
+    let groupName;
+
+    if (this.state.editableGroupName === null)
+      groupName = this.props.group.name;
+    else
+      groupName = this.state.editableGroupName;
+
     return (
       <article className="group">
-        <span className="group__button-more fas fa-ellipsis-v" onClick={this.onButtonMoreClick.bind(this)}/>
-        <a href={`groups/${this.props.group.id}`} className="group__link">
+        <section className="group__info"
+                 style={this.state.isEditGroupFormVisible ? {display: 'none'} : {}}>
+          <span className="group__button-more fas fa-ellipsis-v" onClick={this.onButtonMoreClick.bind(this)}/>
+          <a href={`groups/${this.props.group.id}`} className="group__link">
+            <div>
+              <span className="group__learned-words">{this.props.group.countLearnedWords}</span>
+              <span className="group__slash">/</span>
+              <span className="group__words-in-group">{this.props.group.countWords}</span>
+            </div>
+          </a>
           <div>
-            <span className="group__learned-words">{this.props.group.countLearnedWords}</span>
-            <span className="group__slash">/</span>
-            <span className="group__words-in-group">{this.props.group.countWords}</span>
+            <span className="group__name">{this.props.group.name}</span>
           </div>
-        </a>
-        <div>
-          <span className="group__name">{this.props.group.name}</span>
-        </div>
-        <div className="group__actions-menu" style={this.state.isActionsMenuVisible ? {} : {display: 'none'}}>
-          <div className="group__actions-menu-item">
-            <span className="group__actions-menu-item-name">Add card</span>
+          <div className="group__actions-menu" style={this.state.isActionsMenuVisible ? {} : {display: 'none'}}>
+            <div className="group__actions-menu-item">
+              <span className="group__actions-menu-item-name">Add card</span>
+            </div>
+            <div className="group__actions-menu-item"
+                 onClick={this.onRenameMenuItemClick.bind(this)}>
+              <span className="group__actions-menu-item-name">Rename</span>
+            </div>
+            <div className="group__actions-menu-item">
+              <span className="group__actions-menu-item-name">Delete</span>
+            </div>
           </div>
-          <div className="group__actions-menu-item">
-            <span className="group__actions-menu-item-name">Rename</span>
-          </div>
-          <div className="group__actions-menu-item">
-            <span className="group__actions-menu-item-name">Delete</span>
-          </div>
-        </div>
+        </section>
+        <section className="group__edit-form"
+                 style={this.state.isEditGroupFormVisible ? {} : {display: 'none'}}>
+          <form>
+            <div className="group__form-body">
+              <textarea name="groupName"
+                        placeholder="Group name"
+                        className="group__field-name"
+                        value={groupName}
+                        onChange={this.onGroupNameChanged.bind(this)}/>
+              <div>
+                <span className="group__field-name-hint">
+                  {`${groupName.length}/${GROUP_NAME_MAX_LENGTH} symbols`}
+                </span>
+              </div>
+            </div>
+            <footer className="group__form-footer">
+              <div>
+                <button type="button"
+                        className="group__form-button group__negative-button"
+                        onClick={this.onCancelButtonClick.bind(this)}>
+                  Cancel
+                </button>
+              </div>
+              <div>
+                <button type="submit"
+                        className="group__form-button group__positive-button"
+                        onClick={this.onEditButtonClick.bind(this)}>
+                  Edit
+                </button>
+              </div>
+            </footer>
+          </form>
+        </section>
       </article>
     )
   }
@@ -49,6 +97,37 @@ class Group extends React.Component {
     this.setState({
       ...this.state,
       isActionsMenuVisible: !this.state.isActionsMenuVisible
+    });
+  }
+
+  onRenameMenuItemClick() {
+    this.setState({
+      ...this.state,
+      isEditGroupFormVisible: true,
+      isActionsMenuVisible: false
+    });
+  }
+
+  onEditButtonClick() {
+    this.setState({
+      ...this.state,
+      isEditGroupFormVisible: true,
+      editableGroupName: null
+    });
+  }
+
+  onCancelButtonClick() {
+    this.setState({
+      ...this.state,
+      isEditGroupFormVisible: false,
+      editableGroupName: null
+    });
+  }
+
+  onGroupNameChanged(event) {
+    this.setState({
+      ...this.state,
+      editableGroupName: event.target.value.slice(0, GROUP_NAME_MAX_LENGTH)
     });
   }
 
