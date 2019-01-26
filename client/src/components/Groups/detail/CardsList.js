ca310ca58987a1c805822399eee4a88203927239
dmitrykologrivko <dmitrykologrivko@gmail.com>
2019-01-26 19:18:16 +0300

    Added card list actions layout

--- client/src/components/Groups/detail/CardsList.js
+++ client/src/components/Groups/detail/CardsList.js
@@ -9,7 +9,12 @@ class CardsList extends React.Component {
     return (
       <div>
         <div className="cards-list-actions">
-
+          <input type="checkbox"/>
+          <div className="cards-list-actions__title">39 cards selected</div>
+          <div className="cards-list-actions__remove-action">
+            <span className="cards-list-actions__remove-action-icon fas fa-trash-alt"/>
+            <span className="cards-list-actions__remove-action-label">Remove</span>
+          </div>
         </div>
         <ul className="cards-list">
           <Card/>
