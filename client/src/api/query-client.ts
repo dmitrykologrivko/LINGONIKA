3112cbeee302abe9f89bd11b48eb1f6f78af3f88
Dmitry Kologrivko <dmitrykologrivko@gmail.com>
2025-01-22 16:01:22 +0300

    Limit retries

--- client/src/api/query-client.ts
+++ client/src/api/query-client.ts
@@ -1,10 +1,27 @@
+import { AxiosError } from 'axios';
 import { QueryClient } from '@tanstack/react-query';
+
+const MAX_RETRIES = 2;
 
 export function createQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
         refetchOnWindowFocus: false,
+        retry: (failureCount, error) => {
+          if (failureCount === MAX_RETRIES) {
+            return false;
+          }
+
+          if (error instanceof AxiosError) {
+            const { status } = error;
+            if (status && status < 500) {
+              return false;
+            }
+          }
+
+          return true;
+        },
       },
     },
   });
