45adb757c6adc5ad3e9b5a29d44afe7571fa2740
Dmitry Kologrivko <dmitrykologrivko@gmail.com>
2025-07-24 20:55:26 +0300

    Set extended query parser

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,16 @@
 import { join } from 'path';
-import { INestApplication } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { Bootstrap, ServeStaticExpressLoader } from '@nestjs-boilerplate/core';
 import { NotFoundExceptionFilter } from './shared';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  await new Bootstrap(AppModule).startApplication({
-    onInit: async (container: INestApplication) => {
+  await new Bootstrap(AppModule).startApplication<NestExpressApplication>({
+    onInit: async (container: NestExpressApplication) => {
       container.enableCors();
       container.useGlobalFilters(new NotFoundExceptionFilter());
+      // https://docs.nestjs.com/migration-guide#query-parameters-parsing
+      container.set('query parser', 'extended');
       return Promise.resolve();
     },
     loaders: [new ServeStaticExpressLoader(join(process.cwd(), 'public'), '')],
